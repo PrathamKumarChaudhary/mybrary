9e3a0b36bf8f7621adcbe4d382c9dc622e806d58
pratham@gmail.com <pratham@gmail.com>
2019-08-23 00:20:17 -0500

    Initial author routes

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
+
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+
+
+
 mongoose.connect(process.env.DATABASE_URL, {
    useNewUrlParser:true})
 const db = mongoose.connection
 db.on('error', error => console.error(error));
-db.once('open',() => console.log('Connected to Mongoose'))
+db.once('open',() => console.log('Connected to Mongoose'));
 
 
 const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
+
 
 
 app.set('view engine','ejs')
@@ -20,9 +27,13 @@ app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 
-app.use(express.static('public'))
-app.use('/', indexRouter)
+app.use(express.static('public'));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}));
 
+app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
 
 
 app.listen(process.env.PORT || 3000)
